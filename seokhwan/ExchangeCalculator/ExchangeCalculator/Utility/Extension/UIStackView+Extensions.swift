2e57448b6607b33a2be7a03bccba0dda90d3320b
youseokhwan <youseokhwan15@gmail.com>
2025-04-18 13:41:19 +0900

    refactor: variadic parameter와 array parameter를 모두 지원하도록 개선

--- seokhwan/ExchangeCalculator/ExchangeCalculator/Utility/Extension/UIStackView+Extensions.swift
+++ seokhwan/ExchangeCalculator/ExchangeCalculator/Utility/Extension/UIStackView+Extensions.swift
@@ -2,6 +2,10 @@ import UIKit
 
 extension UIStackView {
     func addArrangedSubviews(_ views: UIView...) {
+        addArrangedSubviews(views)
+    }
+
+    func addArrangedSubviews(_ views: [UIView]) {
         views.forEach {
             addArrangedSubview($0)
         }
--- seokhwan/ExchangeCalculator/ExchangeCalculator/Utility/Extension/UIView+Extensions.swift
+++ seokhwan/ExchangeCalculator/ExchangeCalculator/Utility/Extension/UIView+Extensions.swift
@@ -2,6 +2,10 @@ import UIKit
 
 extension UIView {
     func addSubviews(_ views: UIView...) {
+        addSubviews(views)
+    }
+
+    func addSubviews(_ views: [UIView]) {
         views.forEach {
             addSubview($0)
         }
