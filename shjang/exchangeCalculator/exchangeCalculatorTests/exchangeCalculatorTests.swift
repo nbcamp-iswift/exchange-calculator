d0dca915c0917cf0a946d624c3eb4323f5714ece
Seungho Jang <sjang1594@gmail.com>
2025-04-22 14:03:04 +0900

    test: mock 추가 및 test

---
+++ shjang/exchangeCalculator/exchangeCalculatorTests/exchangeCalculatorTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+final class ExchangeCalculatorTests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
